7e98e49dcc232a9b43575c60a59245913cc37654
Kavinda <kavindadewmith@gmail.com>
2025-05-14 13:39:29 +0530

    fix: Update API_BASE_URL to use backendUrl from config

--- src/utils/apiHelper.tsx
+++ src/utils/apiHelper.tsx
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import type { Stop, Journey, JourneyResponse } from './type';
+import { backendUrl } from './config'; // Adjust the import based on your project structure
 
 // API base URL - this should be from your environment variables in a real app
-const API_BASE_URL = 'http://localhost:5000'; // adjust as needed
+const API_BASE_URL =  backendUrl// adjust as needed
 
 // Get all stops
 export const getStops = async (): Promise<Stop[]> => {
